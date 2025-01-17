72e1898828e3d8f4d87e05ba1440869e7e5836d8
Smit Patel <smit.patel@enlightvision.com>
2025-01-17 15:16:49 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+
+# load the model from disk
+modle_path = "model/model.pkl"
+with open(modle_path, "rb") as file:
+    model = pickle.load(file)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # extrect data from form
+    age = request.form['age']
+    gender = request.form['gender']
+    stream = request.form['stream']
+    internships = request.form['internships']
+    CGPA = request.form['CGPA']
+    historyOfBacklogs = request.form['historyOfBacklogs']
+
+    int_features = [int(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+
+    # make prediction
+    prediction = model.predict(final_features)
+    output = "Placed" if prediction[0] == 1 else "Not Placed"
+
+    return render_template('index.html', age=age, gender=gender, stream=stream, internships=internships,
+                           CGPA=CGPA, historyOfBacklogs=historyOfBacklogs, prediction_text=output)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.preprocessing import LabelEncoder
+import pickle
+import numpy as np
+import sklearn
+
+print("sklearn :",sklearn.__version__)
+print("numpy :", np.__version__)
+print("pandas :",pd.__version__)
+
+file_path = "placement.csv"
+data = pd.read_csv(file_path)
+
+label_encoders = {}
+for column in data.select_dtypes(include=['object']).columns:
+    le = LabelEncoder()
+    data[column] = le.fit_transform(data[column])
+    label_encoders[column] = le
+
+# Display encoded data
+print("\nEncoded Data:")
+
+
+data = data.drop(columns=['Hostel'])
+
+print(data)
+print(data.info())
+print(data['Stream'].unique())
+
+# x = data.drop(['Status'], axis=1)
+# y = data['Status']
+
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+# param_grid = {
+#     'n_estimators': [50, 100],
+#     'max_depth': [ 10, 20],
+#     'min_samples_split': [2, 5],
+#     'min_samples_leaf': [1, 2]
+#     }
+
+# rf = RandomForestClassifier(random_state=42)
+# grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=3, n_jobs=-1, verbose=2)
+# grid_search.fit(x_train, y_train)
+
+# best_rf = grid_search.best_estimator_
+
+# y_pred = best_rf.predict(x_test)
+# print("Accuracy: ", accuracy_score(y_test, y_pred))
+# print("Classification Report: ", classification_report(y_test, y_pred))
+
+# with open("model.pkl", "wb") as file:
+#     pickle.dump(best_rf, file)  
